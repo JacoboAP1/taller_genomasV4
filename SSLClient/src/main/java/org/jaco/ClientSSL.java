7e5722337ed671fc7761a8c0437d0d53d982979b
JacoboAP1 <jacobo.arroyavep@autonoma.edu.co>
2025-09-08 14:28:30 -0500

    Se deja funcionando la comunicación SSL entre client y server

--- SSLClient/src/main/java/org/jaco/ClientSSL.java
+++ SSLClient/src/main/java/org/jaco/ClientSSL.java
@@ -1,4 +1,22 @@
 package org.jaco;
 
+import javax.net.ssl.*;
+import java.io.*;
+
 public class ClientSSL {
+    public void start() throws Exception {
+        System.setProperty("javax.net.ssl.trustStore", "C:/Users/arroy/OneDrive/Documentos/6to semestre/back/taller_genomasV4/certs/keyserver.p12");
+        System.setProperty("javax.net.ssl.trustStorePassword", "123456");
+
+        SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+        try (SSLSocket socket = (SSLSocket) factory.createSocket("localhost", 8443)) {
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            out.println("Hola servidor SSL");
+
+            String response = in.readLine();
+            System.out.println("Servidor respondió: " + response);
+        }
+    }
 }
--- SSLServer/src/main/java/org/jaco/ServerSSL.java
+++ SSLServer/src/main/java/org/jaco/ServerSSL.java
@@ -1,4 +1,38 @@
 package org.jaco;
 
+import javax.net.ssl.*;
+import java.io.*;
+import java.security.KeyStore;
+
 public class ServerSSL {
+    public void start() throws Exception {
+        String keystorePath = "C:/Users/arroy/OneDrive/Documentos/6to semestre/back/taller_genomasV4/certs/keyserver.p12";
+        char[] password = "123456".toCharArray();
+
+        KeyStore ks = KeyStore.getInstance("PKCS12");
+        ks.load(new FileInputStream(keystorePath), password);
+
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+        kmf.init(ks, password);
+
+        SSLContext sc = SSLContext.getInstance("TLS");
+        sc.init(kmf.getKeyManagers(), null, null);
+
+        SSLServerSocketFactory ssf = sc.getServerSocketFactory();
+        SSLServerSocket serverSocket = (SSLServerSocket) ssf.createServerSocket(8443);
+
+        System.out.println("Servidor SSL escuchando en puerto 8443...");
+
+        while (true) {
+            try (SSLSocket socket = (SSLSocket) serverSocket.accept()) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+                String msg = in.readLine();
+                System.out.println("Cliente dice: " + msg);
+
+                out.println("Hola cliente, soy el servidor SSL");
+            }
+        }
+    }
 }
